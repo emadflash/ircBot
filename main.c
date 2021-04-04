1c7f943013b22203df627777292e6161f1347950
madflash <backbugkaught@gmail.com>
2021-04-05 00:20:31 +0530

    abstracted client side sockets in utils.h; error: stack-overflow-error if large buffer size is not specified which is larger then previous iterations

--- main.c
+++ main.c
@@ -1,70 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<arpa/inet.h>
-#include<netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define RECV_BUFSIZE 4096
 
 #include "./irc.h"
-#include "./parse.h"
+#include "./utils.h"
 
+char *irc_node = "irc.freenode.net";
 const int port = 6667;
-char *node = "irc.freenode.net";
+
+void callback(int socket_fd, char *buffer, size_t len) {
+  /* if receive ping request then send authentication token
+   */
+  if (is_pong_request(buffer) == 0) {
+    if (irc_pong(socket_fd, buffer, RECV_BUFSIZE) < 0) {
+      perror("irc_pong failed");
+    }
+  }
+}
 
 int main() {
-	struct sockaddr_in addr;
-	struct hostent *host;
-	
-	// create socket:
-    	int server = socket(AF_INET, SOCK_STREAM, 0);
-	if(server < 0){
-        	perror("socket() failed");
-		exit(EXIT_FAILURE);
-    	}
-	
-	host = gethostbyname(node);
-	printf("[*] connecting to %s\n", host->h_name);
-	
-	// set ip and port
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = *(unsigned int*)host->h_addr;
-	addr.sin_port = htons((unsigned short)port);
+  client_sock_t *client = (client_sock_t *)calloc(1, sizeof(client_sock_t));
+  strcpy(client->ip, irc_node);
+  client->port = port;
 
-	// connect to server
-	if (connect(server, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("connect() failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	LOGIN_INFO* c = LOGIN_INFO_NEW();
-	
-	if ((irc_login(server, c)) < 0) {
-		perror("irc_login() failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	LOGIN_INFO_DESTROY(c);
-	
-	if ((irc_join(server, "#networking")) < 0) {
-		perror("irc_join() failed");
-	}
+  if (client_sock_t_initialize(client) < 0) {
+    perror("client_sock_t_initialize() failed");
+    exit(EXIT_FAILURE);
+  }
+  // connect to server
+  if (client_sock_t_connect(client)) {
+    perror("client_sock_t_connect() failed");
+    exit(EXIT_FAILURE);
+  }
 
-	char buffer[RECV_BUFSIZE];
-	while(1) {
-		memset(buffer, '\0', RECV_BUFSIZE);	// make sure buffer[] is empty
-		irc_recv(server, buffer, RECV_BUFSIZE);
-		if (is_pong_request(buffer) == 0) {     // check if auth request is sent
-			if (irc_pong(server, buffer, RECV_BUFSIZE) < 0) {
-				perror("irc_pong failed");
-			}
-		}
-		printf("%s", buffer);
-	}
-	close(server);	
-	
-	return 0;
+  LOGIN_INFO *c = LOGIN_INFO_NEW();
+
+  if ((irc_login(client->socket_fd, c)) < 0) {
+    perror("irc_login() failed");
+    exit(EXIT_FAILURE);
+  }
+
+  LOGIN_INFO_DESTROY(c);
+
+  if ((irc_join(client->socket_fd, "#networking")) < 0) {
+    perror("irc_join() failed");
+  }
+
+  client_sock_t_listen(client, callback);
+  client_sock_t_destroy(client);
+  return 0;
 }
